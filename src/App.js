516644958719c818cdef55f2cd8d836f64430c5f
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-03-24 09:41:34 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero';
 import About from './components/About';
 import Support from './components/Support';
-
+import Platforms from './components/Platforms';
+import Pricing from './components/Pricing.jsx';
+import Footer from './components/Footer.jsx';
 
 const App = () => {
   return (
@@ -13,6 +15,9 @@ const App = () => {
         <Hero />
         <About />
         <Support />
+        <Platforms />
+        <Pricing />
+        <Footer />
     </div>
   )
 }
