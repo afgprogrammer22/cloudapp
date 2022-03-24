daddf72c388522ba02edf571be7cb1bc885710d6
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-03-24 08:20:09 +0530

    Navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    const handleClick = () => setNav(!nav);
+
   return (
-    <div>Navbar</div>
+    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+        <div className="flex justify-between items-center px-2 w-full h-full">
+            <div className='flex items-center'>
+                <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>CLOUD.</h1>
+                <ul className="hidden md:flex">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Support</li>
+                    <li>Platforms</li>
+                    <li>Pricing</li>
+                </ul>
+            </div>
+            <div className='hidden md:flex pr-4'>
+                <button className='border-none bg-transparent text-white mr-4'>Sign In</button>
+                <button className='px-8 py-3'>Sign Up</button>
+            </div>
+            <div className='md:hidden' onClick={handleClick}>
+                {!nav ? (<MenuIcon className='w-5' />) 
+                    : (<XIcon className='w-5' />)
+                }
+                
+            </div>
+        </div>
+
+        <ul className={!nav ? 'hidden' : 'absolute px-8 w-full bg-zinc-200'}>
+            <li className='border-b-2 border-zinc-300 w-full'>Home</li>
+            <li className='border-b-2 border-zinc-300 w-full'>About</li>
+            <li className='border-b-2 border-zinc-300 w-full'>Support</li>
+            <li className='border-b-2 border-zinc-300 w-full'>Platforms</li>
+            <li className='border-b-2 border-zinc-300 w-full'>Pricing</li>
+            <div className='flex flex-col my-4'>
+                <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign In</button>
+                <button className='px-8 py-3'>Sign Up</button>
+            </div>
+        </ul>
+
+    </div>
   )
 }
 
