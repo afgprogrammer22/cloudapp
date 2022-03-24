c90ec45c1ae3fce29ba6ab4dde4d3ff40447a42c
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-03-24 08:38:28 +0530

    Hero section completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './index.css';
 import Navbar from './components/Navbar.jsx';
+import Hero from './components/Hero';
 
 
 const App = () => {
   return (
     <div>
         <Navbar />
+        <Hero />
     </div>
   )
 }
